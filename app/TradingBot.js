4871e826d2baa14246d7ce3ebc5e659379375355
RVLareu <rlareu@fi.uba.ar>
2022-04-30 13:08:55 -0300

    added a constructor a setCoinValue to bot

--- app/TradingBot.js
+++ app/TradingBot.js
@@ -1,3 +1,10 @@
 export class TradingBot {
-    
+    constructor(initial_coin_value) {
+        this.wallet = null
+        this.coinsValue = initial_coin_value
+    }
+
+    setCoinsValue(new_coin_value) {
+        this.coinsValue = new_coin_value
+    }
 }
