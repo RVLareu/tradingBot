6c9a34830e70c2c7b8602323035da2a356369f10
RVLareu <rlareu@fi.uba.ar>
2022-04-30 13:45:06 -0300

    added methods to bot (buying selling)

--- app/TradingBot.js
+++ app/TradingBot.js
@@ -1,3 +1,5 @@
+import { Wallet } from "./Wallet.js"
+
 export class TradingBot {
     constructor(initial_coin_value) {
         this.wallet = null
@@ -6,5 +8,33 @@ export class TradingBot {
 
     setCoinsValue(new_coin_value) {
         this.coinsValue = new_coin_value
+    }
+
+    // biased coin, market value should increase 60% of the time
+    updateCoinsMarketValue() {
+        if (Math.random() > 0.4) {
+            this.coinsValue += 1
+        } else {
+            this.coinsValue -= 1
+        }
+    }
+
+    associateWallet(wallet) {
+        this.wallet = wallet
+    }
+
+    buyCoins() {
+        var balanceAvailable = this.wallet.balance
+        var coinsTobuy = balanceAvailable / this.coinsValue
+        this.wallet.takeFromBalance(balanceAvailable)
+        this.wallet.addToCoins(coinsTobuy)
+    }
+
+
+    sellCoins() {
+        var coinsAvailable = this.wallet.coins
+        var earningsFromSellingCoins = coinsAvailable * this.coinsValue
+        this.wallet.addToBalance(earningsFromSellingCoins)
+        this.wallet.takeFromCoins(coinsAvailable)
     }
 }
