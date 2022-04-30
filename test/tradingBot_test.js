50eee9fa3d788d510ba7e795b628e8393c5ac94e
RVLareu <rlareu@fi.uba.ar>
2022-04-30 13:44:36 -0300

    test when buying/selling for trading bot

--- test/tradingBot_test.js
+++ test/tradingBot_test.js
@@ -1,4 +1,5 @@
 import { TradingBot } from "../app/TradingBot.js";
+import { Wallet } from "../app/Wallet.js";
 import {expect} from "chai"
 
 describe("Trading Bot", () => {
@@ -16,12 +17,63 @@ describe("Trading Bot", () => {
     })
 
     describe("Setting values: ", () => {
-
         it("coins value should be updated after changing it", () => {
             const tradingBot = new TradingBot(0)
             tradingBot.setCoinsValue(10)
             const coinsValue = tradingBot.coinsValue
             expect(coinsValue).to.equal(10)
+        })
+        it("Should associate a wallet after setting it", () => {
+            const tradingBot = new TradingBot(0)
+            const wallet = new Wallet()
+            tradingBot.associateWallet(wallet)
+            const botWallet = tradingBot.wallet
+            expect(botWallet).to.equal(wallet)
+        })
+    })
+
+    describe("Bot updating wallet after purchasing/selling coins: ", () => {
+        it("Should buy all coins possible updating the balance and coins", () => {
+            const tradingBot = new TradingBot(1)
+            const wallet = new Wallet()
+            tradingBot.associateWallet(wallet)
+
+            wallet.setBalance(10)
+            tradingBot.buyCoins()
+            
+            var balance = wallet.balance
+            var coins = wallet.coins
+
+            expect(balance).to.equal(0)
+            expect(coins).to.equal(10)
+        })
+        it("Shouldnt buy coins when the balance is zero", () => {
+            const tradingBot = new TradingBot(10)
+            const wallet = new Wallet()
+            tradingBot.associateWallet(wallet)
+
+            wallet.setBalance(0)
+            tradingBot.buyCoins()
+
+            var balance = wallet.balance
+            var coins = wallet.coins
+
+            expect(balance).to.equal(0)
+            expect(coins).to.equal(0)
+        })
+        it("Should sell all coins possible updating the balance and coins", () => {
+            const tradingBot = new TradingBot(1)
+            const wallet = new Wallet()
+            tradingBot.associateWallet(wallet)
+
+            wallet.setCoins(10)
+            tradingBot.sellCoins()
+            
+            var balance = wallet.balance
+            var coins = wallet.coins
+
+            expect(balance).to.equal(10)
+            expect(coins).to.equal(0)
         })
     })
 })
