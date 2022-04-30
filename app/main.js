ae9f8dd1a5f319716cb5e8da213c69c2349aee2a
RVLareu <rlareu@fi.uba.ar>
2022-04-30 15:19:15 -0300

    prints and days modified in main script

--- app/main.js
+++ app/main.js
@@ -8,25 +8,26 @@ tradingBot.associateWallet(wallet)
 
 var win = 0
 var loss = 0
+var final_net_worth = 0
+
 
 for (let practice = 0; practice < 100; practice++) {
-    for (let day = 1; day < 11; day++) {
-        console.log("DAY " + day)
+    for (let day = 1; day < 100; day++) {
         tradingBot.updateCoinsMarketValue()
         tradingBot.trade()
         
     }
     
-    console.log("FINAL BALANCE: ", wallet.balance)
-    console.log("FINAL COINS: ", wallet.coins)
-    console.log("FINAL COINS VALUE IN WALLET: ", wallet.coins * tradingBot.coinsValue)
-    console.log("FINAL NET WORTH: ", wallet.balance + (wallet.coins * tradingBot.coinsValue))
-    if (wallet.balance + (wallet.coins * tradingBot.coinsValue >= 10000)) {
+    final_net_worth += wallet.balance + (wallet.coins * tradingBot.coinsValue)
+    if (wallet.balance + (wallet.coins * tradingBot.coinsValue) >= 10000) {
         win += 1;
     } else {
         loss += 1;
     }
 }
+
+console.log("AFTER RUNNING 100 TIMES A TRADING BOT DURING 100 DAYS STARTING WITH BALANCE 10000: ")
 console.log("WINS: ", win)
 console.log("LOSSES: ", loss)
+console.log("FINAL AVERAGE NET WORTH: ", final_net_worth/100)
     
