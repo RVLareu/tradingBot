a684c1121a467e252f69a42bca990536e2eb6d5d
RVLareu <rlareu@fi.uba.ar>
2022-04-30 13:09:19 -0300

    added test for bot (creating, setting)

--- test/tradingBot_test.js
+++ test/tradingBot_test.js
@@ -1,2 +1,27 @@
 import { TradingBot } from "../app/TradingBot.js";
 import {expect} from "chai"
+
+describe("Trading Bot", () => {
+    describe("Creating the Trading Bot: ", () => {
+        it("Should be created with no wallet asociated", () => {
+            const tradingBot = new TradingBot(0)
+            const tradingWallet = tradingBot.wallet
+            expect(tradingWallet).to.equal(null)
+        })
+        it("Should be created with the coins initial value passed by argument", () => {
+            const tradingBot = new TradingBot(10)
+            const coinsValue = tradingBot.coinsValue
+            expect(coinsValue).to.equal(10)
+        })
+    })
+
+    describe("Setting values: ", () => {
+
+        it("coins value should be updated after changing it", () => {
+            const tradingBot = new TradingBot(0)
+            tradingBot.setCoinsValue(10)
+            const coinsValue = tradingBot.coinsValue
+            expect(coinsValue).to.equal(10)
+        })
+    })
+})
